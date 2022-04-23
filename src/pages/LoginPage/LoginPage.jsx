228474a8b214e19457fbaa6b426e013378dd31da
TusharJade <79801546+TusharJade@users.noreply.github.com>
2022-04-23 22:46:35 +0530

    feat: added login and signup page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,53 @@
 import "./LoginPage.css";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  return <></>;
+  return (
+    <>
+      <div className="sidebar-box">
+        <Sidebar />
+      </div>
+      <form className="login-outerbox">
+        <div className="login-container">
+          <div className="login-text">Login</div>
+
+          <div className="input-container">
+            <label className="login-heading" for="/">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input-feild"
+            ></input>
+          </div>
+
+          <div className="input-container password-container">
+            <label className="login-heading" for="/">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input-feild"
+            ></input>
+          </div>
+
+          <div className="or-text">OR</div>
+
+          <button className="guest-login-btn">Login as Guest</button>
+
+          <div className="new-text">
+            Don't have an account yet?
+            <Link to="/signup" className="signup-text Link">
+              &nbsp;Signup
+            </Link>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export { LoginPage };
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,7 +1,61 @@
-import "./SignupPage.css";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="sidebar-box">
+        <Sidebar />
+      </div>
+      <form className="login-outerbox">
+        <div className="login-container">
+          <div className="login-text">Signup</div>
+
+          <div className="input-container">
+            <label className="login-heading" for="/">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input-feild"
+            ></input>
+          </div>
+
+          <div className="input-container password-container">
+            <label className="login-heading" for="/">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input-feild"
+            ></input>
+          </div>
+
+          <div className="input-container password-container">
+            <label className="login-heading" for="/">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              placeholder=" Confirm Password"
+              className="input-feild"
+            ></input>
+          </div>
+
+          <button className="guest-login-btn singup-gap">Signup</button>
+
+          <div className="new-text">
+            Already have an account?
+            <Link to="/login" className="signup-text Link">
+              &nbsp;Login
+            </Link>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export { SignupPage };
