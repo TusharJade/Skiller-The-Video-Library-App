0ef4397d9774fa418e7f18854f82230e17b05e33
TusharJade <79801546+TusharJade@users.noreply.github.com>
2022-05-23 03:48:54 +0530

    refactor: changed data

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -8,6 +8,61 @@ import { v4 as uuid } from "uuid";
 export const videos = [
   {
     _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/7bWdJX4dnew/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/7bWdJX4dnew",
+    description:
+      "Video Editing Before and After: After Effects Behind the Scenes (VFX) | Roy Adin",
+    creatorsLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLRYftWJ9WQqfbidiFpDQlwaDZZh85EXjRq2s1DCMUk=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Roy Adin",
+    premiumCreator: true,
+    category: "video editing",
+    views: "46k",
+    time: "2 years",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/6pY7EjqD3QA/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/6pY7EjqD3QA",
+    description:
+      "The four-letter code to selling anything | Derek Thompson | TEDxBinghamtonUniversity",
+    creatorsLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLQGnY6McntoY-eWaRDI047FWFqj7pVlw2nBfkwz7w=s88-c-k-c0x00ffffff-no-rj",
+    creator: "TEDx Talks",
+    premiumCreator: true,
+    category: "sales",
+    views: "2.6M",
+    time: "3 years",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/byDNMLTuOqI/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/byDNMLTuOqI",
+    description:
+      "‘Colour’ Visual element of Graphic Design / Design theory Ep3/45 [Beginners guide to Graphic Design]",
+    creatorsLogo:
+      "https://yt3.ggpht.com/y5Nj-jmdjcz7vfoeb6zunz69t2uOsunzPGT7DVOzSMB_hL1N4EGozgcdCGOihINB9Y_nBOIi6A=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Gareth David Studio",
+    premiumCreator: true,
+    category: "graphic design",
+    views: "325k",
+    time: "2 years",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/N5Zi_G2MTHw/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/N5Zi_G2MTHw",
+    description: "Why Listening is Better Than Talking | Brian Tracy",
+    creatorsLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLSR1-iR0FfdYGEqZj5slytUw7z628az9maQ05SQNg=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Brian Tracy",
+    premiumCreator: true,
+    category: "communication",
+    views: "55k",
+    time: "1 year",
+  },
+  {
+    _id: uuid(),
     thumbnailImg: "https://img.youtube.com/vi/ujaCbzLwuB8/maxresdefault.jpg",
     videoLink: "https://www.youtube.com/embed/ujaCbzLwuB8",
     description:
@@ -20,6 +75,20 @@ export const videos = [
     views: "666k",
     time: "6 year",
     date: "13 Jun 2016",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/d536ca4BzRs/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/d536ca4BzRs",
+    description:
+      "How To Sell ANYTHING to ANYONE in HINDI | बेचने की कला सीखो ! Sales Motivation !",
+    creatorsLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLTAoqk7oUQEP-jf3bZh1embvt-G2qyyI6XM09o9UA=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Ranveer Allahbadia",
+    premiumCreator: true,
+    category: "sales",
+    views: "2.1M",
+    time: "10 months",
   },
   {
     _id: uuid(),
@@ -38,6 +107,20 @@ export const videos = [
   },
   {
     _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/oS1BlGm91Bs/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/oS1BlGm91Bs",
+    description:
+      "Learn Video Editing Full Course For Beginners Step By Step Guide",
+    creatorsLogo:
+      "https://yt3.ggpht.com/tC7T8-4MvARqycrxkYbYQh5ygYMjqIn4ET9s-OAjmWeM349qscepubONTcBvb_vG43jwW_Xi_g=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Technology Gyan",
+    premiumCreator: true,
+    category: "video editing",
+    views: "2M",
+    time: "1 years",
+  },
+  {
+    _id: uuid(),
     thumbnailImg: "https://img.youtube.com/vi/5V4uuNdmRHc/maxresdefault.jpg",
     videoLink: "https://www.youtube.com/embed/5V4uuNdmRHc",
     description: "Top 10 Composition Tips - Photography Course 3/10",
@@ -49,6 +132,20 @@ export const videos = [
     views: "1.2M",
     time: "6 year",
     date: "11 May 2016",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/dFSia1LZI4Y/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/dFSia1LZI4Y",
+    description:
+      "What is Graphic Design? Ep1/45 [Beginners Guide to Graphic Design]",
+    creatorsLogo:
+      "https://yt3.ggpht.com/y5Nj-jmdjcz7vfoeb6zunz69t2uOsunzPGT7DVOzSMB_hL1N4EGozgcdCGOihINB9Y_nBOIi6A=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Gareth David Studio",
+    premiumCreator: true,
+    category: "graphic design",
+    views: "1.6M",
+    time: "2 years",
   },
   {
     _id: uuid(),
@@ -67,6 +164,19 @@ export const videos = [
   },
   {
     _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/HAnw168huqA/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/HAnw168huqA",
+    description: "Think Fast, Talk Smart: Communication Techniques",
+    creatorsLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLRSMpZ9D2BtDJbw9rL4jBf7TvEZaDFgZ49b_Ah16Q=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Stanford",
+    premiumCreator: true,
+    category: "communication",
+    views: "44k",
+    time: "11 months",
+  },
+  {
+    _id: uuid(),
     thumbnailImg: "https://img.youtube.com/vi/LxO-6rlihSg/maxresdefault.jpg",
     videoLink: "https://www.youtube.com/embed/LxO-6rlihSg",
     description:
@@ -79,32 +189,6 @@ export const videos = [
     views: "2.3M",
     time: "2 years",
     date: "20 Dec 2016",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/N5Zi_G2MTHw/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/N5Zi_G2MTHw",
-    description: "Why Listening is Better Than Talking | Brian Tracy",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLSR1-iR0FfdYGEqZj5slytUw7z628az9maQ05SQNg=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Brian Tracy",
-    premiumCreator: true,
-    category: "communication",
-    views: "55k",
-    time: "1 year",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/HAnw168huqA/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/HAnw168huqA",
-    description: "Think Fast, Talk Smart: Communication Techniques",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRSMpZ9D2BtDJbw9rL4jBf7TvEZaDFgZ49b_Ah16Q=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Stanford",
-    premiumCreator: true,
-    category: "communication",
-    views: "44k",
-    time: "11 months",
   },
   {
     _id: uuid(),
@@ -122,6 +206,32 @@ export const videos = [
   },
   {
     _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/ywwg-H1otaY/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/ywwg-H1otaY",
+    description: "SALES Is Just Like DATING | Simon Sinek",
+    creatorsLogo:
+      "https://yt3.ggpht.com/ytc/AKedOLRWK-7SEE7uTtp3PTvRHClj1yGpZLruhkD8pyGoQw=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Simon Sinek",
+    premiumCreator: false,
+    category: "sales",
+    views: "169k",
+    time: "1 years",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/-6Tt46y6nAs/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/-6Tt46y6nAs",
+    description: "Top 5 Best Video Editing Apps⚡January 2022",
+    creatorsLogo:
+      "https://yt3.ggpht.com/EYXEN3n0cfYgs_zDqlSVk8JY1y6gLrzfmdV17uce1zkMEKRoH7VlLwaptPBh6zBk_ie-c7Qw=s88-c-k-c0x00ffffff-no-rj",
+    creator: "rakin Tech",
+    premiumCreator: true,
+    category: "video editing",
+    views: "864k",
+    time: "1 years",
+  },
+  {
+    _id: uuid(),
     thumbnailImg: "https://img.youtube.com/vi/xr1q-uBtIH4/maxresdefault.jpg",
     videoLink: "https://www.youtube.com/embed/xr1q-uBtIH4",
     description: "5 Ways to Improve your COMMUNICATION Skills",
@@ -131,6 +241,20 @@ export const videos = [
     premiumCreator: true,
     category: "communication",
     views: "2.7M",
+    time: "2 years",
+  },
+  {
+    _id: uuid(),
+    thumbnailImg: "https://img.youtube.com/vi/5jprIWG8f5g/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/5jprIWG8f5g",
+    description:
+      "‘Shape’ Visual element of Graphic Design / Design theory Ep4/45 [Beginners guide to Graphic Design]",
+    creatorsLogo:
+      "https://yt3.ggpht.com/y5Nj-jmdjcz7vfoeb6zunz69t2uOsunzPGT7DVOzSMB_hL1N4EGozgcdCGOihINB9Y_nBOIi6A=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Gareth David Studio",
+    premiumCreator: true,
+    category: "graphic design",
+    views: "266`k",
     time: "2 years",
   },
   {
@@ -148,87 +272,31 @@ export const videos = [
   },
   {
     _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/h6eeDgBjZq8/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/h6eeDgBjZq8",
+    thumbnailImg: "https://img.youtube.com/vi/c1sKrhPwC24/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/c1sKrhPwC24",
     description:
-      "Before you Start Learning Adobe Premiere Pro CC Class 1 - Urdu / Hindi",
+      "Freelance Video Editor: Earn Rs. 2000/hr! | How to Become a Video Editor",
     creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRBWZ1bFZGXA-YBFY9dYHh0CdW_fsYvDc3oQIJvaQ=s88-c-k-c0x00ffffff-no-rj",
-    creator: "GFXMentor",
+      "https://yt3.ggpht.com/5NTOTidzy2R9HriIqLFcXdCL26eTgqz3_iKBFqTTOYY2lzWzBOlsI8nx8Vt4dobCA3x8ABXKLsU=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Ishan Sharma",
     premiumCreator: true,
     category: "video editing",
-    views: "3.3M",
-    time: "2 years",
+    views: "111k",
+    time: "2 months",
   },
   {
     _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/TP8wre-Mm1k/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/TP8wre-Mm1k",
+    thumbnailImg: "https://img.youtube.com/vi/ZUKLrTl22gg/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/ZUKLrTl22gg",
     description:
-      "Video Details & Project Window - Adobe Premiere Pro CC Class 2 - Urdu / Hindi",
+      "Sales Skills And Techniques Explained In Hindi | Ranveer Allahbadia",
     creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRBWZ1bFZGXA-YBFY9dYHh0CdW_fsYvDc3oQIJvaQ=s88-c-k-c0x00ffffff-no-rj",
-    creator: "GFXMentor",
+      "https://yt3.ggpht.com/ytc/AKedOLSbIq4XmLWjHnDXQB-8lUgih_aQ5cFabXp2yIVMVw=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Ranveer Allahbadia",
     premiumCreator: true,
-    category: "video editing",
-    views: "1M",
-    time: "2 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/kCGNe7BFq6g/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/kCGNe7BFq6g",
-    description:
-      "Source Window & Download FREE Videos - Adobe Premiere Pro CC Class 3 - Urdu / Hindi",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRBWZ1bFZGXA-YBFY9dYHh0CdW_fsYvDc3oQIJvaQ=s88-c-k-c0x00ffffff-no-rj",
-    creator: "GFXMentor",
-    premiumCreator: true,
-    category: "video editing",
-    views: "864k",
-    time: "2 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/2TyL6ViQDwQ/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/2TyL6ViQDwQ",
-    description:
-      "New Sequence and Timeline - Adobe Premiere Pro CC Class 4 - Urdu / Hindi",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRBWZ1bFZGXA-YBFY9dYHh0CdW_fsYvDc3oQIJvaQ=s88-c-k-c0x00ffffff-no-rj",
-    creator: "GFXMentor",
-    premiumCreator: true,
-    category: "video editing",
-    views: "864k",
-    time: "2 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/wDbosNeayeA/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/wDbosNeayeA",
-    description:
-      "Selection and Track Selection Tools - Adobe Premiere Pro CC Class 5 - Urdu / Hindi",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRBWZ1bFZGXA-YBFY9dYHh0CdW_fsYvDc3oQIJvaQ=s88-c-k-c0x00ffffff-no-rj",
-    creator: "GFXMentor",
-    premiumCreator: true,
-    category: "video editing",
-    views: "864k",
-    time: "2 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/dFSia1LZI4Y/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/dFSia1LZI4Y",
-    description:
-      "What is Graphic Design? Ep1/45 [Beginners Guide to Graphic Design]",
-    creatorsLogo:
-      "https://yt3.ggpht.com/y5Nj-jmdjcz7vfoeb6zunz69t2uOsunzPGT7DVOzSMB_hL1N4EGozgcdCGOihINB9Y_nBOIi6A=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Gareth David Studio",
-    premiumCreator: true,
-    category: "graphic design",
-    views: "1.6M",
-    time: "2 years",
+    category: "sales",
+    views: "2.3M",
+    time: "1 years",
   },
   {
     _id: uuid(),
@@ -246,31 +314,16 @@ export const videos = [
   },
   {
     _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/byDNMLTuOqI/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/byDNMLTuOqI",
-    description:
-      "‘Colour’ Visual element of Graphic Design / Design theory Ep3/45 [Beginners guide to Graphic Design]",
+    thumbnailImg: "https://img.youtube.com/vi/fOBd_hLOnGI/maxresdefault.jpg",
+    videoLink: "https://www.youtube.com/embed/fOBd_hLOnGI",
+    description: "How to Edit Video Fast in Adobe Premiere Pro | Rachit Singh",
     creatorsLogo:
-      "https://yt3.ggpht.com/y5Nj-jmdjcz7vfoeb6zunz69t2uOsunzPGT7DVOzSMB_hL1N4EGozgcdCGOihINB9Y_nBOIi6A=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Gareth David Studio",
+      "https://yt3.ggpht.com/ytc/AKedOLTyqgtsRL1H85VOwtipJ-bXzA3s5w0NMJAptmfQrg=s88-c-k-c0x00ffffff-no-rj",
+    creator: "Rachit Singh",
     premiumCreator: true,
-    category: "graphic design",
-    views: "325k",
-    time: "2 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/5jprIWG8f5g/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/5jprIWG8f5g",
-    description:
-      "‘Shape’ Visual element of Graphic Design / Design theory Ep4/45 [Beginners guide to Graphic Design]",
-    creatorsLogo:
-      "https://yt3.ggpht.com/y5Nj-jmdjcz7vfoeb6zunz69t2uOsunzPGT7DVOzSMB_hL1N4EGozgcdCGOihINB9Y_nBOIi6A=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Gareth David Studio",
-    premiumCreator: true,
-    category: "graphic design",
-    views: "266`k",
-    time: "2 years",
+    category: "video editing",
+    views: "86k",
+    time: "1 years",
   },
   {
     _id: uuid(),
@@ -298,60 +351,5 @@ export const videos = [
     category: "sales",
     views: "1.3M",
     time: "6 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/6pY7EjqD3QA/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/6pY7EjqD3QA",
-    description:
-      "The four-letter code to selling anything | Derek Thompson | TEDxBinghamtonUniversity",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLQGnY6McntoY-eWaRDI047FWFqj7pVlw2nBfkwz7w=s88-c-k-c0x00ffffff-no-rj",
-    creator: "TEDx Talks",
-    premiumCreator: true,
-    category: "sales",
-    views: "2.6M",
-    time: "3 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/ZUKLrTl22gg/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/ZUKLrTl22gg",
-    description:
-      "Sales Skills And Techniques Explained In Hindi | Ranveer Allahbadia",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLSbIq4XmLWjHnDXQB-8lUgih_aQ5cFabXp2yIVMVw=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Ranveer Allahbadia",
-    premiumCreator: true,
-    category: "sales",
-    views: "2.3M",
-    time: "1 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/ywwg-H1otaY/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/ywwg-H1otaY",
-    description: "SALES Is Just Like DATING | Simon Sinek",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLRWK-7SEE7uTtp3PTvRHClj1yGpZLruhkD8pyGoQw=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Simon Sinek",
-    premiumCreator: false,
-    category: "sales",
-    views: "169k",
-    time: "1 years",
-  },
-  {
-    _id: uuid(),
-    thumbnailImg: "https://img.youtube.com/vi/d536ca4BzRs/maxresdefault.jpg",
-    videoLink: "https://www.youtube.com/embed/d536ca4BzRs",
-    description:
-      "How To Sell ANYTHING to ANYONE in HINDI | बेचने की कला सीखो ! Sales Motivation !",
-    creatorsLogo:
-      "https://yt3.ggpht.com/ytc/AKedOLTAoqk7oUQEP-jf3bZh1embvt-G2qyyI6XM09o9UA=s88-c-k-c0x00ffffff-no-rj",
-    creator: "Ranveer Allahbadia",
-    premiumCreator: true,
-    category: "sales",
-    views: "2.1M",
-    time: "10 months",
   },
 ];
